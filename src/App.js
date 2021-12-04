5337034d1cb4275161eb1abbc55306370e2aad4e
InNomineDeorum <pointingrave@gmail.com>
2021-12-04 17:33:48 +0300

    Added a basic file stucture and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+
+  const messageVariable = 'big ass bootie';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Hello from React</h1>
+      <h1>Hello from {messageVariable}</h1>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className="logo">
+                    <h1>HG</h1>
+                </div>
+                    <ul className="nav-bar">
+                        <li>home</li>
+                        <li>about us</li>
+                        <li>friends</li>
+                        <li>my profile</li>
+                    </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
