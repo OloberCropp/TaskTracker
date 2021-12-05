2c54b845f3b24a3e72caa749869def3b8043b6fd
InNomineDeorum <pointingrave@gmail.com>
2021-12-05 11:02:00 +0300

    continue working.. nothing interesting yet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import Header from './components/Header';
 
 function App() {
 
-  const messageVariable = 'big ass bootie';
-
   return (
     <div className="App">
       <Header />
-      <h1>Hello from React</h1>
-      <h1>Hello from {messageVariable}</h1>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
-function Header() {
+function Header( props ) {
     return (
         <header>
             <nav className='container'>
                 <div className="logo">
                     <h1>HG</h1>
                 </div>
-                    <ul className="nav-bar">
-                        <li>home</li>
-                        <li>about us</li>
-                        <li>friends</li>
-                        <li>my profile</li>
+                    <ul className="nav-links">
+                        <li>{props.title}</li>
+                        <li>{props.title1}</li>
+                        <li>{props.title2}</li>
+                        <li>{props.title3}</li>
                     </ul>
             </nav>
         </header>
     )
+}
+
+
+Header.defaultProps = {
+    title: 'home',
+    title1: 'about us',
+    title2: 'friends',
+    title3: 'my profile'
 }
 
 export default Header
