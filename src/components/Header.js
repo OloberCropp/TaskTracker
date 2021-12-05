2affa29aaf1389303540008a86bb1993cf3ef8c9
InNomineDeorum <pointingrave@gmail.com>
2021-12-05 11:20:27 +0300

    testing further

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 function Header( props ) {
     return (
-        <header>
-            <nav className='container'>
-                <div className="logo">
-                    <h1>HG</h1>
-                </div>
-                    <ul className="nav-links">
-                        <li>{props.title}</li>
-                        <li>{props.title1}</li>
-                        <li>{props.title2}</li>
-                        <li>{props.title3}</li>
-                    </ul>
-            </nav>
+        <header className='container'>
+            <div className="logo">
+                <h1>HG</h1>
+            </div>
+                <ul className="nav-links">
+                    <li>{props.title}</li>
+                    <li>{props.title1}</li>
+                    <li>{props.title2}</li>
+                    <li>{props.title3}</li>
+                </ul>
         </header>
     )
 }
